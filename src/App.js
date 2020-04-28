6c089fcd14b6b21f0a5d3a1a7145948943121f9f
prashant707 <prashantgahlot707@gmail.com>
2020-04-29 01:07:35 +0530

    updating a app with shoppage , collection preview component and collection-item component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Switch,Route,Link } from 'react-router-dom'
 
 import './App.css';
-
+import ShopPage from './pages/shop/shop.component'
 
 import HomePage from './pages/homepage/homepage.component'
 
@@ -52,7 +52,7 @@ function App() {
 
  
   <Route  exact path='/' component={HomePage} />
-  <Route exact path='/hats' component={HatsPage} />
+  <Route exact path='/shop' component={ShopPage} />
   
   
   
